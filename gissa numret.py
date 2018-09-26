a2e83bb7b589a5a5e6598a1a1291c8563344c363
Pontus Erlingsson <erpo16@nti.umea>
2018-09-26 13:16:18 +0200

    update

---
+++ gissa numret.py
@@ -0,0 +1,11 @@
+import random
+ai = random,rand(1,4)
+print("gissa på ett av valen från menyn")
+
+meny = 0 
+
+while != 5:
+    print("tryck på 1 för att gissa på hjärter ♥ ")
+    print("tryck på 2 för att gissa på spader ♠ ")
+    print("tryck på 3 för att gissa på klöver ♣ ")
+    print("tryck på 4 för att gissa på ruter ♦ ")
